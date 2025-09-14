55d290c4b738aebc5ebce5c713412926ec2ba5a2
Santiago Gutierrez Correa <124370013+Santi-a-ux@users.noreply.github.com>
2025-09-14 17:17:43 -0500

    Instantiate BaseDeDatos before creating table

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import sys
 sys.path.append("src")
+
 from view_web.flask_app import Run
 from controller.controlador import BaseDeDatos
+
 if __name__ == "__main__":
-    BaseDeDatos.crear_tabla()
-    Run.app.run(host='127.0.0.1', port=8080, debug=True)
+    bd = BaseDeDatos()
+    bd.crear_tabla()
+    Run.app.run(host="127.0.0.1", port=8080, debug=True)
